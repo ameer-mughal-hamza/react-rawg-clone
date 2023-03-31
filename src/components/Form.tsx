290a6c9150a88a4f5d01772f236aa9df2a0b5d0c
ameer-mughal-hamza <hamza6417307@gmail.com>
2023-03-31 15:29:39 +0200

    React forms using react-forms-hook

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,49 +1,47 @@
 import { ChangeEvent, FormEvent, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { FieldValue, FieldValues } from "react-hook-form/dist/types";
 import Button from "./Button/Button";
 
-interface IPerson {
+interface FormData {
   name: string;
   age: number;
 }
 
 const Form = () => {
-  // const name = useRef<HTMLInputElement>(null);
-  // const age = useRef<HTMLInputElement>(null);
-  // const person: { name: string; age: number } = { name: "", age: 0 };
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(person);
-  };
-
-  const [person, setPerson] = useState<IPerson>({ name: "", age: 0 });
-
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+  const onSubmit = (data: FieldValues) => console.log(data);
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
-          value={person.name}
-          onChange={(event) =>
-            setPerson({ ...person, name: event.target.value })
-          }
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name field is required.</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">The name must be atleast 3 characters.</p>
+        )}
       </div>
       <div className="mb-3">
-        <label htmlFor="name" className="form-label">
+        <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          id="name"
+          id="age"
           type="number"
-          value={person.age}
-          onChange={(event) =>
-            setPerson({ ...person, age: +event.target.value })
-          }
+          {...register("age")}
           min={18}
           max={80}
           maxLength={2}
