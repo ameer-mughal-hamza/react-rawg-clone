53ea087bc8b07b7f00289665722f36e529d4cdf6
ameer-mughal-hamza <hamza6417307@gmail.com>
2023-04-01 11:13:39 +0200

    Install Chakra UI.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-import "./App.css";
-import Alert from "./components/Alert/Alert";
-import Button from "./components/Button/Button";
-
 function App() {
-  return (
-    <>
-      <Alert show={true} color="info">
-        This is an alert.
-      </Alert>
-      <Button color="primary">Hello World</Button>
-    </>
-  );
+  
 }
 
 export default App;
