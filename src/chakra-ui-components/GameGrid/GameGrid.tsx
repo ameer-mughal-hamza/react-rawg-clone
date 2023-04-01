b1a2b3280ed2628510056da90f306f867940fb74
ameer-mughal-hamza <hamza6417307@gmail.com>
2023-04-01 16:03:31 +0200

    Custom hook to fetch games and cleanup function. Added error handling.

--- src/chakra-ui-components/GameGrid/GameGrid.tsx
+++ src/chakra-ui-components/GameGrid/GameGrid.tsx
@@ -1,37 +1,10 @@
 import React, { useEffect, useState } from "react";
+import useGames from "../../hooks/useGames";
 import apiClient from "../../services/apiClient";
 import "./GameGrid.module.css";
-interface RawgGameResult {
-  id: number;
-  name: string;
-  slug: string;
-}
-
-interface RawgGame {
-  count: number;
-  next: string;
-  previous: string;
-  results: RawgGameResult[];
-}
 
 const GameGrid = () => {
-  const [games, setGames] = useState<RawgGameResult[]>([]);
-  const [error, setErrors] = useState("");
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    apiClient
-      .get<RawgGame>("/games")
-      .then((res) => {
-        setIsLoading(false);
-        setGames(res.data.results);
-      })
-      .catch((err) => {
-        setErrors(err.message);
-        setIsLoading(false);
-      });
-  }, []);
+  const { games, error, isLoading } = useGames();
 
   return (
     <div>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,48 @@
+import { CanceledError } from "axios";
+import { useState, useEffect } from "react";
+import apiClient from "../services/apiClient";
+
+export interface RawgGameResult {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+export interface RawgGame {
+  count: number;
+  next: string;
+  previous: string;
+  results: RawgGameResult[];
+}
+
+function useGames() {
+  const [games, setGames] = useState<RawgGameResult[]>([]);
+  const [error, setErrors] = useState("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setIsLoading(true);
+    apiClient
+      .get<RawgGame>("/games", { signal: controller.signal })
+      .then((res) => {
+        setIsLoading(false);
+        setGames(res.data.results);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) {
+          return;
+        }
+
+        setErrors(err.message);
+        setIsLoading(false);
+      });
+
+    // Cleanup function
+    return () => controller.abort();
+  }, []);
+
+  return { games, error, isLoading, setGames, setErrors, setIsLoading };
+}
+
+export default useGames;
